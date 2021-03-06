09ac278014db7ecd5c8c2b272bfeab861f856c13
dk-kling <deokk1112@gmail.com>
2021-03-06 20:30:45 +0900

    Add client.authorize

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 const {google} = require('googleapis');
 const keys = require('./keys.json');
 
+// google auth for JSON Web Token
+const client = new google.auth.JWT(
+    keys.client_email, 
+    null, 
+    keys.private_key, 
+    ['https://www.googleapis.com/auth/spreadsheets']
+);
 
+client.authorize(function(err, tokens){
+    if(err) {
+        console.log(err);
+        return;
+    }
+    else {
+        console.log('Connected!');
+    }
+});
