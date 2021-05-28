3f62413eecb2c1530e852c3941dc98da045326e8
dk-kling <deokk1112@gmail.com>
2021-05-28 22:57:45 +0900

    Add commit/rollback re

--- main.js
+++ main.js
@@ -24,60 +24,72 @@ function command() {
     rl.prompt();
     rl.on('line', (line) => {
         try {
-            const ast = parser.astify(line);
-            switch (ast.type) {
-                case 'create':
-                    switch (ast.keyword) {
-                        case 'database':
-                            console.log("complete");
-                            console.log(ast);
-                            const database = ast.database;
-                            rl.close();
-                            break;
-                        case 'table':
-                            console.log("complete");
-                            // console.log(ast);
-                            for (var i in ast.table) {
-                                console.log(ast.table[i].table);
-                            }
-                            for (var i in ast.create_definitions) {
-                                console.log(ast.create_definitions[i].column);
-                                console.log(ast.create_definitions[i].definition);
-                                console.log(ast.create_definitions[i].nullable);
-                            }
-                            rl.close();
-                            break;
-                    }
-                    break;
-
-                case 'select':
-                    console.log("select!!!");
-                    console.log(ast);
-                    rl.close();
-                    break;
-
-                case 'drop':
-                    console.log("drop!!!");
-                    console.log(ast);
-                    rl.close();
-                    break;
-
-                case 'delete':
-                    console.log("delete!!!");
-                    console.log(ast);
-                    rl.close();
-                    break;
-
-                case 'update':
-                    console.log("update!!!");
-                    console.log(ast);
-                    rl.close();
-                    break;
-
-                default:
-                    rl.close();
-                    console.log(ast);
-                    break;
+            let commit_re = /commit(\s*)(;?)/
+            let rollback_re = /rollback(\s*)(;?)/
+            if (commit_re.test(line.toLowerCase())){
+                console.log('commit!!');
+                rl.close();
+            }
+            else if (rollback_re.test(line.toLowerCase())) {
+                console.log('rollback!!');
+                rl.close();
+            }
+            else {
+                const ast = parser.astify(line);
+                switch (ast.type) {
+                    case 'create':
+                        switch (ast.keyword) {
+                            case 'database':
+                                console.log("complete");
+                                console.log(ast);
+                                const database = ast.database;
+                                rl.close();
+                                break;
+                            case 'table':
+                                console.log("complete");
+                                // console.log(ast);
+                                for (var i in ast.table) {
+                                    console.log(ast.table[i].table);
+                                }
+                                for (var i in ast.create_definitions) {
+                                    console.log(ast.create_definitions[i].column);
+                                    console.log(ast.create_definitions[i].definition);
+                                    console.log(ast.create_definitions[i].nullable);
+                                }
+                                rl.close();
+                                break;
+                        }
+                        break;
+    
+                    case 'select':
+                        console.log("select!!!");
+                        console.log(ast);
+                        rl.close();
+                        break;
+    
+                    case 'drop':
+                        console.log("drop!!!");
+                        console.log(ast);
+                        rl.close();
+                        break;
+    
+                    case 'delete':
+                        console.log("delete!!!");
+                        console.log(ast);
+                        rl.close();
+                        break;
+    
+                    case 'update':
+                        console.log("update!!!");
+                        console.log(ast);
+                        rl.close();
+                        break;
+    
+                    default:
+                        rl.close();
+                        console.log(ast);
+                        break;
+                }
             }
         }
         catch (err) {
